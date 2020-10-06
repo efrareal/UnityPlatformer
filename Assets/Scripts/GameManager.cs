49fd968e4b56f175cce95a0cb4b41080e8b83da9
efrareal <efrareal@gmail.com>
2020-10-06 18:18:26 -0500

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager sharedInstance;
+
+    public GameState currentGameState = GameState.menu;
+
+    public GameObject uiTitlePanel;
+    public GameObject gameOverUIPanel;
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    private void Start()
+    {
+        BackToMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("s") && currentGameState != GameState.inGame)
+        {
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+        PlayerController.sharedInstance.StartGame();
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        if(newGameState == GameState.menu)
+        {
+            uiTitlePanel.SetActive(true);
+            gameOverUIPanel.SetActive(false);
+        }
+        else if(newGameState == GameState.inGame)
+        {
+            uiTitlePanel.SetActive(false);
+            gameOverUIPanel.SetActive(false);
+        }
+        else if(newGameState == GameState.gameOver)
+        {
+            gameOverUIPanel.SetActive(true);
+            uiTitlePanel.SetActive(false);
+        }
+
+        this.currentGameState = newGameState;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController sharedInstance;
+
+    private float runningSpeed = 14f;
+    private float jumpForce = 20f;
+    public LayerMask groundLayer;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sharedInstance = this;
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        startPosition = this.transform.position;
+    }
+
+    public void StartGame()
+    {
+        _animator.SetBool("Idle", false);
+        _animator.SetBool("isGrounded", true);
+        this.transform.position = startPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Jump();
+            }
+            _animator.SetBool("isGrounded", IsTouchingGround());
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if (_rb.velocity.x < runningSpeed)
+            {
+                _rb.velocity = new Vector2(runningSpeed, _rb.velocity.y);
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        if (IsTouchingGround())
+        {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+
+    private bool IsTouchingGround()
+    {
+        //RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, 2.2f, groundLayer);
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 2.3f, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Kill()
+    {
+        GameManager.sharedInstance.GameOver();
+        this._animator.SetBool("Idle", true);
+    }
+}
